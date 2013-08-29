67d169dc015a1695c19c2ea967abd44ed478bca9
Mark Hendrickson <mhndrcksn@gmail.com>
2013-08-29 23:46:41 +1000

    heh

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>Ginevra Morgan</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="css/flat-ui.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+<!-- custom CSS Links -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<!-- My links -->
+	<link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+<div class="centerfy">
+	<a href="http://www.linkedin.com/in/ginevramorgan">
+	<div class="brandcontain">
+	<img src="/images/G-white.png">
+	</div>
+	</a>
+	<h1 class="bigquote">Ginevra Morgan</h1>
+	<div class="lilnote">
+		<h4>A personal homepage for Ginevra, coming soon. Made with love by Mark.</h4>
+	</div>
+</div>
+
+<div id="background">
+</div>
+
+    <!-- Load JS here for greater good =============================-->
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script src="js/jquery.ui.touch-punch.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/bootstrap-switch.js"></script>
+    <script src="js/flatui-checkbox.js"></script>
+    <script src="js/flatui-radio.js"></script>
+    <script src="js/jquery.tagsinput.js"></script>
+    <script src="js/jquery.placeholder.js"></script>
+    <script src="js/jquery.stacktable.js"></script>
+    <script src="http://vjs.zencdn.net/c/video.js"></script>
+    <script src="js/application.js"></script>
+
+</body>
+</html>
+
